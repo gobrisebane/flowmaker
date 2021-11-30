434ea966edf7cc8309f738d5b5a4ba7d75814951
twepo <gobrisebane@gmail.com>
2021-11-30 21:27:43 +0930

    TEST쪽 수정

--- src/test/java/com/twepo/flowmaker/FlowmakerApplicationTests.java
+++ src/test/java/com/twepo/flowmaker/FlowmakerApplicationTests.java
@@ -3,7 +3,7 @@ package com.twepo.flowmaker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FlowmakerApplicationTests {
    
    @Test
