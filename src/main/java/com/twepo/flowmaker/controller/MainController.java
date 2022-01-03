bd1cb9537920d4c2f0afe2bd23e8eb463a77cd55
twepo <gobrisebane@gmail.com>
2022-01-03 21:57:49 +0930

    travis 세팅

--- src/main/java/com/twepo/flowmaker/controller/MainController.java
+++ src/main/java/com/twepo/flowmaker/controller/MainController.java
@@ -9,22 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
    
    
-   /*
-   * NEW VERSION ERA.. NEED TO IMPROVE
-   *
-   * */
-   
-   /*
-   * vol2 rebase test 2
-   *
-   * by choi
-   * */
+
    
    @GetMapping("/")
    public String index(Model model){
-	  System.out.println("main_branch_test");
-	  return "main_braS";
+		
+		
+	  return "main/index";
+	  
+	  
+	  
    }
+	
+	
 
 
 }
