a790cad0554a9b53c78222327c40fc920701ac2d
twepo <gobrisebane@gmail.com>
2022-01-10 19:45:39 +0930

    deply.sh 재장전

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,7 +30,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR Name 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
@@ -39,7 +39,7 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.yaml,
-    classpath:application-real.yaml,/home/ec2-user/app/application-real-db.yaml \
+    classpath:/application-real.yaml,/home/ec2-user/app/application-real-db.yaml \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
